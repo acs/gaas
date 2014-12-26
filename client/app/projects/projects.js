67d5ed74fbd7e82fe09924b153f052c21a5bf33b
Alvaro del Castillo <acs@bitergia.com>
2014-12-26 18:46:46 +0100

    Added features to add new projects and new github repos.

--- client/app/projects/projects.js
+++ client/app/projects/projects.js
@@ -20,5 +20,25 @@ angular.module('gaasClientApp.projects', ['ngRoute'])
         });
     };
 
+    $scope.create_project = function() {
+        var name = $scope.dash_selected['project_new'];
+        $scope.projects[name] = {"source":[],"trackers":[]};
+    };
+
+    $scope.add_repo = function() {
+        var name = $scope.dash_selected['project_new'];
+        var repo_url = $scope.dash_selected['source_url'];
+        var project = $scope.projects[$scope.dash_selected['project']];
+        var scm = '"https://github.com/' + repo_url + '"';
+        var its = '"https://api.github.com/repos/' + repo_url + '/issues"';
+        project.source.push(scm);
+        project.trackers.push(its);
+    };
+
+
     $scope.load_projects();
+    $scope.dashes = {
+            "github":{"name":"github",file:"github_test.conf"},
+            "bugzilla":{"name":"bugzilla",file:"bugzilla_test.conf"}
+    }
 }]);
